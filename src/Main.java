cf42e11407dc98206a32e6ddfa0a74b58dfcbdfb
Riba33 <riba331845@gmail.com>
2021-02-10 15:52:36 +0200

    Создал первый класс

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World! 123");
+        System.out.println();
+        System.out.println("Еще одна строка в тексте!");
+    }
+}
